98fb7772f936373975b71112d0c9f4e09c52f50d
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-05-08 20:42:46 +0400

    Add main view collection director

--- EasyTodo/Sources/Extensions/Typealiases.swift
+++ EasyTodo/Sources/Extensions/Typealiases.swift
@@ -4,3 +4,5 @@ typealias VoidClosure = () -> Void
 typealias ParameterClosure<T> = (T) -> Void
 typealias DataSource<T1: Hashable, T2: Hashable> = UICollectionViewDiffableDataSource<T1, T2>
 typealias SupplementaryItem = NSCollectionLayoutBoundarySupplementaryItem
+typealias SupplementaryViewProvider<T1: Hashable, T2: Hashable> = UICollectionViewDiffableDataSource<T1, T2>.SupplementaryViewProvider
+typealias SupplementaryRegistration<T: UICollectionReusableView> = UICollectionView.SupplementaryRegistration<T>
